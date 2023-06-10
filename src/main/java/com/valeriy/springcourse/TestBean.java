a31a30090f6feb66414c0725e9e9142fcb5fe7ac
Valeriy Cherkashin <valeriycherkashin@yahoo.com>
2023-06-10 13:44:56 -0400

    init commit

---
+++ src/main/java/com/valeriy/springcourse/TestBean.java
@@ -0,0 +1,18 @@
+package com.valeriy.springcourse;
+
+public class TestBean {
+    String name;
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
